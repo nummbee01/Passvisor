64fc65d2eaf49e8745314f47300ca6b344351822
num <149131001+nummbee01@users.noreply.github.com>
2025-08-15 10:45:43 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import json
+
+# Page configuration
+st.set_page_config(page_title="Passvisor",page_icon="🔐")
+
+st.title("Passvisor 🔐")
+st.write("AI-powered Password Analyzer")
+
+st.success("App running.")
+
+# Sidebar information
+st.sidebar.markdown("---")
+
+#
